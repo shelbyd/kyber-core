302fa0c5526c6f73641af38847589911f65cface
Shelby Doolittle <shelby@shelbyd.com>
2018-11-20 19:27:31 -0500

    clean up main project

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,8 @@
 extern crate failure;
-extern crate kyber_support;
 extern crate structopt;
 
 use failure::Error;
-use structopt::StructOpt;
 
-mod plugin;
-mod plugin_locator;
-
-use kyber_support::{Arguments, Options, PluginAction};
-
-fn main() {
-    match run() {
-        Ok(()) => {}
-        Err(e) => {
-            println!("{}", e);
-            std::process::exit(1)
-        }
-    }
-}
-
-fn run() -> Result<(), Error> {
-    let arguments = Arguments::from_args();
-    match arguments {
-        Arguments::Options(options) => {
-            let actions = get_actions(&options)?;
-            for action in actions {
-                println!("{}", action);
-            }
-        }
-        Arguments::Do { .. } => {}
-    }
-
+fn main() -> Result<(), Error> {
     Ok(())
-}
-
-fn get_actions(options: &Options) -> Result<Vec<PluginAction>, Error> {
-    Ok(plugin_locator::path_exes()?
-        .into_iter()
-        .flat_map(|plugin| plugin.actions(&options.filename, &options.line_col))
-        .collect::<Vec<_>>())
 }
