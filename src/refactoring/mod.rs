0d7b5d3f469e76b5692863d7ddaaba7152df8750
Shelby Doolittle <shelby@shelbyd.com>
2018-12-02 21:04:49 -0500

    only inline on word boundaries

--- src/refactoring/mod.rs
+++ src/refactoring/mod.rs
@@ -1,19 +1,23 @@
 use containing_scope::*;
 use failure::Error;
+use regex::*;
 use replace_range::*;
 
 pub fn inline_variable(contents: &str, start: usize, end: usize) -> Result<String, Error> {
     let variable_name = &contents[start..=end];
-    let expression_matcher = regex::Regex::new(&format!("let {} = (?P<expr>.+);", variable_name))?;
+    let expression_matcher = Regex::new(&format!("let {} = (?P<expr>.+);", variable_name))?;
+    let variable_matcher = Regex::new(&format!("\\b{}\\b", variable_name))?;
 
     let range = start..(end + 1);
 
     Ok(contents
         .replace_range(containing_scope(contents, range), |s| {
             let expression = &expression_matcher.captures(&s).unwrap()["expr"];
-            expression_matcher
-                .replace(&s, "")
-                .replace(variable_name, expression)
+            let expression = format!("({})", expression);
+            let without_expression = expression_matcher.replace(s, "").to_string();
+            variable_matcher
+                .replace_all(&without_expression, expression.as_str())
+                .to_string()
         })
         .unwrap())
 }
